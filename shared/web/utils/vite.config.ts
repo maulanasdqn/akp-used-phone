bf2ef813a00278a7249022c18d2d5c38c216b93b
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2025-05-27 10:03:44 +0700

    fix: several issue

--- shared/web/utils/vite.config.ts
+++ shared/web/utils/vite.config.ts
@@ -16,12 +16,6 @@ export default defineConfig(() => ({
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
     }),
   ],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
     outDir: '../../../dist/shared/web/utils',
     emptyOutDir: true,
@@ -30,16 +24,12 @@ export default defineConfig(() => ({
       transformMixedEsModules: true,
     },
     lib: {
-      // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
       name: 'utils',
       fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
       formats: ['es' as const],
     },
     rollupOptions: {
-      // External packages that should not be bundled into your library.
       external: [],
     },
   },
